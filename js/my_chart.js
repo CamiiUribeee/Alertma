30a2a13fe45d4cf9524dd83d7204ffff57f5700f
Kevin192212 <kppardo0401@gmail.com>
2024-05-15 14:15:40 -0500

    graphic

--- js/my_chart.js
+++ js/my_chart.js
@@ -1,28 +1,27 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Define riskPercentage antes de usarla
-    const riskPercentage = 30; // Cambia este valor por el valor correcto según tu lógica
+   
+    const riskPercentage = 30; 
 
     const ctx = document.getElementById('myChart').getContext('2d');
-    const data = {
-        labels: ['Tu Riesgo', 'Mujeres de Tu Edad', 'Historial Familiar', 'Promedio General'],
-        datasets: [{
-            label: 'Porcentaje de Riesgo',
-            data: [riskPercentage, 20, 50, 10],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'
-            ],
+    console.log(ctx); 
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
             borderWidth: 1
-        }]
-    };
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
 
     const config = {
         type: 'bar',
@@ -37,5 +36,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
     };
 
     // Crear la gráfica
-    const myChart = new myChart(ctx, config);
+    //const myChart = new myChart(ctx, config);
 });
